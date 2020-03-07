44fc1f5a118efc2989d044dea9b2cba8b4a45d98
JCGuest <jcguest90@gmail.com>
2020-03-07 01:26:05 -0600

    challenge 1: use the api to add four imgs to page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+addEventListener('DOMContentLoaded', function() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        let message = json.message
+        message.forEach( v => insertImg(v));
+    })
+})
+
+function insertImg(v) {
+    let dogContainer = document.querySelector("#dog-image-container")
+    let dogImage = document.createElement("img")
+    dogImage.src = v
+    dogContainer.appendChild(dogImage)
+}
