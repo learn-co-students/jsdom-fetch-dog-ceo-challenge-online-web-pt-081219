5e06ed78e3477991c497595aa0d0c15f5510811e
JCGuest <jcguest90@gmail.com>
2020-03-09 15:54:35 -0500

    completed challenge 4, added random color generator if li is clicked

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-//Challenge 1
-// addEventListener('DOMContentLoaded', function() {
-//     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-//     fetch(imgUrl)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(json) {
-//         let message = json.message
-//         message.forEach( v => insertImg(v));
-//     })
-// })
+// Challenge 1
+addEventListener('DOMContentLoaded', function() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        let message = json.message
+        message.forEach( v => insertImg(v));
+    })
+})
 
-// function insertImg(v) {
-//     let dogContainer = document.querySelector("#dog-image-container")
-//     let dogImage = document.createElement("img")
-//     dogImage.src = v
-//     dogContainer.appendChild(dogImage)
-// }
+function insertImg(v) {
+    let dogContainer = document.querySelector("#dog-image-container")
+    let dogImage = document.createElement("img")
+    dogImage.src = v
+    dogContainer.appendChild(dogImage)
+}
 
-// Challenge 2 & 3
+// Challenge 2 & 3 & 4
 addEventListener('DOMContentLoaded', function() {
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    function populateBreeds() {
     fetch(breedUrl)
     .then(function(response) {
         return response.json();
@@ -33,16 +34,31 @@ addEventListener('DOMContentLoaded', function() {
     .catch(err => {
         console.log(err)
     });
+    }
+    populateBreeds()
 
 
     let breedUl = document.querySelector('#dog-breeds')
+    let dropdown = document.querySelector("#breed-dropdown")
 
     function insertBreed(v) {
         let breed = document.createElement('li')
         breed.innerText = v
+        if (v.charAt(0) === dropdown.options[dropdown.selectedIndex].value) {
         breedUl.appendChild(breed)
+        }
     };
+ 
+    dropdown.addEventListener('click', function(e) {
+        let selection = dropdown.options[dropdown.selectedIndex].value
+        while (breedUl.firstElementChild) {
+            breedUl.removeChild(breedUl.firstElementChild)
+        }
+        populateBreeds()
+    })
 
+    
+/////////////////////////////////////////////////////////////////////////////////
     breedUl.addEventListener('click', function(e) {
         changeColor(e);
     })
@@ -58,4 +74,7 @@ addEventListener('DOMContentLoaded', function() {
         let c = Math.floor(Math.random() * 255)
         return 'rgb(' + a + ',' + b + ',' + c + ')'
     }
+
+
+
 });
