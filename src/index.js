521d257e10cb70972cde074d705698ee4998887c
Shaqqour <mshaqqourm@gmail.com>
2020-03-09 18:27:19 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-console.log('%c HI', 'color: firebrick');
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-const breedUrl = 'https://dog.ceo/api/breeds/list/all';
-const breeds = document.getElementsByClassName("breeds");
+//const breeds = document.getElementsByTagName("li");
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchImages();
+    fetchBreeds();
+});
 
 function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
     return fetch(imgUrl)
     .then(resp => resp.json())
     .then(json => renderImages(json));
 }
-
-function fetchBreeds() {
-    return fetch(breedUrl)
-    .then(resp => resp.json())
-    .then(json => renderBreeds(json))
-}
-
 function renderImages(json) {
     const images = document.getElementById("dog-image-container");
 
     let imagesArray = json["message"];
-    
+
     for (let i = 0; i < imagesArray.length; i++) {
         const img = document.createElement("img");
         img.setAttribute("src", `${imagesArray[i]}`);
         images.appendChild(img);
     }
+}
+
+function fetchBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json))
 }
 
 function renderBreeds(json) {
@@ -34,21 +37,19 @@ function renderBreeds(json) {
 
     for (let i = 0; i < breedsArray.length; i++) {
         const li = document.createElement("li");
-        li.setAttribute("class", "breeds")
         li.innerHTML = `${breedsArray[i]}`;
+        li.style.cursor = 'pointer';
         breeds.appendChild(li);
+        li.addEventListener("click", changeColor);
     }
 }
 
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetchImages();
-    fetchBreeds();
-});
-
-for(let i = 0; i < breeds.length; i++) {
-    breeds[i].addEventListener("click", function(e) {
-        e.preventDefault();
-        breeds[i].innerHTML = "change";
-    });
+function changeColor(event) {
+    let color = event.target.style.color;
+    if (color == "red") {
+        event.target.style.color = "black";
+    }
+    else {
+        event.target.style.color = "red";
+    }
 }
